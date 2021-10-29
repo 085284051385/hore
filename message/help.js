b7fc11e42ba834744560ce977355f28806bd921b
085284051385 <ggridho074@gmail.com>
2021-10-29 19:26:25 +0700

    hore

---
+++ message/help.js
@@ -0,0 +1,262 @@
+exports.islamiMenu = (prefix) => {
+    return `╭─❒ 「 *ISLAMI MENU* 」
+│ ⬡ ${prefix}alquran
+│ ⬡ ${prefix}asmaulhusna
+│ ⬡ ${prefix}alquranaudio
+│ ⬡ ${prefix}jadwalsholat
+│ ⬡ ${prefix}kisahnabi
+│ ⬡ ${prefix}listsurah
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.tebakMenu = (prefix) => {
+    return `╭─❒ 「 *TEBAK MENU* 」
+│ ⬡ ${prefix}tebakumur
+│ ⬡ ${prefix}artinama *nama*
+│ ⬡ ${prefix}jodoh
+│ ⬡ ${prefix}jadian
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.scriptMenu = (prefix) => {
+    return `*➤ BOT Ini Menggunakan Script : bikin dewekk*`
+}
+exports.infoMenu = (prefix) => {
+    return `╭─❒ 「 *INFO MENU* 」
+│ ⬡ ${prefix}kbbi
+│ ⬡ ${prefix}infogempa
+│ ⬡ ${prefix}covidindo
+│ ⬡ ${prefix}covidglobal
+│ ⬡ ${prefix}jadwaltv
+│ ⬡ ${prefix}translate
+│ ⬡ ${prefix}cuaca
+│ ⬡ ${prefix}wikipedia *teks*
+│ ⬡ ${prefix}jarak
+│ ⬡ ${prefix}speed
+│ ⬡ ${prefix}grupbot
+│ ⬡ ${prefix}youtube
+│ ⬡ ${prefix}infoig
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.stalkMenu = (prefix) => {
+    return `╭─❒ 「 *STALK MENU* 」
+│ ⬡ ${prefix}stalkig *nama*
+│ ⬡ ${prefix}stalkgithub *nama*
+│ ⬡ ${prefix}stalktiktok *nama*
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.imageMenu = (prefix) => {
+    return `╭─❒ 「 *IMAGE MENU* 」
+│ ⬡ ${prefix}art
+│ ⬡ ${prefix}loli
+│ ⬡ ${prefix}neko
+│ ⬡ ${prefix}waifu
+│ ⬡ ${prefix}husbu
+│ ⬡ ${prefix}wallnime
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ]* ──────❏
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `╭─❒ 「 *STICKER MENU* 」
+│ ⬡ ${prefix}attp *teks*
+│ ⬡ ${prefix}sticker *reply image*
+│ ⬡ ${prefix}tomp3
+│ ⬡ ${prefix}tovideo
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.searchMenu = (prefix) => {
+    return `╭─❒ 「 *SEARCH MENU* 」
+│ ⬡ ${prefix}gimage
+│ ⬡ ${prefix}wallpapersearch
+│ ⬡ ${prefix}playstore
+│ ⬡ ${prefix}shopee *text*
+│ ⬡ ${prefix}google *text*
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `╭─❒ 「 *OWNER MENU* 」
+│ ⬡ ${prefix}owner
+│ ⬡ ${prefix}sewabot
+│ ⬡ ${prefix}bc *teks*
+│ ⬡ ${prefix}report
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽L ]* ──────❏
+`
+}
+exports.bugMenu = (prefix) => {
+    return `╭─❒ 「 *BANTAI MENU* 」
+│ ⬡ ${prefix}bugpc2
+│ ⬡ ${prefix}bug *jumlah*
+│ ⬡ ${prefix}bughole
+│ ⬡ ${prefix}bugtroli
+│ ⬡ ${prefix}bugtroli2
+│ ⬡ ${prefix}bugcombine
+│ ⬡ ${prefix}buglokasi
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.textMenu = (prefix) => {
+    return `╭─❒ 「 *TEXT MENU* 」
+│ ⬡ ${prefix}quotes
+│ ⬡ ${prefix}quotesanime
+│ ⬡ ${prefix}quotesdilan
+│ ⬡ ${prefix}quotesimage
+│ ⬡ ${prefix}katabijak
+│ ⬡ ${prefix}random nama
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.groupMenu = (prefix) => {
+    return`╭─❒ 「 *MENU GROUP* 」 
+│ ⬡ ${prefix}antilink *on/off*
+│ ⬡ ${prefix}welcome *on/off*
+│ ⬡ ${prefix}antivirtex *on/off*
+│ ⬡ ${prefix}group
+│ ⬡ ${prefix}linkgrup
+│ ⬡ ${prefix}promote *@tag*
+│ ⬡ ${prefix}demote *@tag*
+│ ⬡ ${prefix}add *@tag*
+│ ⬡ ${prefix}kick *@tag*
+│ ⬡ ${prefix}setpp *reply image*
+│ ⬡ ${prefix}setdesc *teks*
+│ ⬡ ${prefix}setname *teks*
+│ ⬡ ${prefix}hidetag *teks*
+│ ⬡ ${prefix}infogc
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `╭─❒ 「 *DOWNLOADER* 」
+│ ⬡ ${prefix}play
+│ ⬡ ${prefix}ytsearch *teks*
+│ ⬡ ${prefix}ytmp3 *link*
+│ ⬡ ${prefix}ytmp4 *link*
+│ ⬡ ${prefix}tiktoknowm *link*
+│ ⬡ ${prefix}tiktokmusic
+│ ⬡ ${prefix}pinterest *teks*
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏`
+}
+exports.allMenu = (prefix, cmhit, runtime, pushname, isOwner, isPremium, gcount, sender, baterai) => {
+    return `╭─❒ 「 MENU GROUP 」 
+│ ⬡ ${prefix}antilink *on/off*
+│ ⬡ ${prefix}welcome *on/off*
+│ ⬡ ${prefix}antivirtex *on/off*
+│ ⬡ ${prefix}group
+│ ⬡ ${prefix}linkgrup
+│ ⬡ ${prefix}promote *@tag*
+│ ⬡ ${prefix}demote *@tag*
+│ ⬡ ${prefix}add *@tag*
+│ ⬡ ${prefix}kick *@tag*
+│ ⬡ ${prefix}setpp *reply image*
+│ ⬡ ${prefix}setdesc *teks*
+│ ⬡ ${prefix}setname *teks*
+│ ⬡ ${prefix}hidetag *teks*
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+
+╭─❒ 「 STICKER MENU 」
+│ ⬡ ${prefix}attp *teks*
+│ ⬡ ${prefix}sticker *reply image*
+│ ⬡ ${prefix}tomp3
+│ ⬡ ${prefix}tovideo
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+╭─❒ 「 DOWNLOADER 」
+│ ⬡ ${prefix}play
+│ ⬡ ${prefix}ytsearch *teks*
+│ ⬡ ${prefix}ytmp3 *link*
+│ ⬡ ${prefix}ytmp4 *link*
+│ ⬡ ${prefix}tiktoknowm *link*
+│ ⬡ ${prefix}tiktokmusic
+│ ⬡ ${prefix}pinterest *teks*
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+╭─❒ 「 ISLAMI MENU 」
+│ ⬡ ${prefix}alquran
+│ ⬡ ${prefix}asmaulhusna
+│ ⬡ ${prefix}alquranaudio
+│ ⬡ ${prefix}jadwalsholat
+│ ⬡ ${prefix}kisahnabi
+│ ⬡ ${prefix}listsurah
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+╭─❒ 「 STICKER MENU 」
+│ ⬡ ${prefix}attp *teks*
+│ ⬡ ${prefix}sticker *reply image*
+│ ⬡ ${prefix}tomp3
+│ ⬡ ${prefix}tovideo
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+╭─❒ 「 INFO MENU 」
+│ ⬡ ${prefix}kbbi
+│ ⬡ ${prefix}infogempa
+│ ⬡ ${prefix}covidindo
+│ ⬡ ${prefix}covidglobal
+│ ⬡ ${prefix}jadwaltv
+│ ⬡ ${prefix}translate
+│ ⬡ ${prefix}cuaca
+│ ⬡ ${prefix}wikipedia *teks*
+│ ⬡ ${prefix}jarak
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+╭─❒ 「 TEXT MENU 」
+│ ⬡ ${prefix}quotes
+│ ⬡ ${prefix}quotesanime
+│ ⬡ ${prefix}quotesdilan
+│ ⬡ ${prefix}quotesimage
+│ ⬡ ${prefix}katabijak
+│ ⬡ ${prefix}random nama
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+╭─❒ 「 SEARCH MENU 」
+│ ⬡ ${prefix}gimage
+│ ⬡ ${prefix}wallpapersearch
+│ ⬡ ${prefix}playstore
+│ ⬡ ${prefix}shopee *text*
+│ ⬡ ${prefix}google *text*
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+╭─❒ 「 TEBAK MENU 」
+│ ⬡ ${prefix}tebakumur
+│ ⬡ ${prefix}artinama *nama*
+│ ⬡ ${prefix}jodoh
+│ ⬡ ${prefix}jadian
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+
+╭─❒ 「 STALK MENU 」
+│ ⬡ ${prefix}stalkig *nama*
+│ ⬡ ${prefix}stalkgithub *nama*
+│ ⬡ ${prefix}stalktiktok *nama*
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+
+╭─❒ 「 IMAGE MENU 」
+│ ⬡ ${prefix}art
+│ ⬡ ${prefix}loli
+│ ⬡ ${prefix}neko
+│ ⬡ ${prefix}waifu
+│ ⬡ ${prefix}husbu
+│ ⬡ ${prefix}wallnime
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+
+╭─❒ 「 BANTAI MENU 」
+│ ⬡ ${prefix}bugpc2
+│ ⬡ ${prefix}bug *jumlah*
+│ ⬡ ${prefix}bughole
+│ ⬡ ${prefix}bugtroli
+│ ⬡ ${prefix}bugtroli2
+│ ⬡ ${prefix}bugcombine
+│ ⬡ ${prefix}buglokasi
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+
+╭─❒ 「 OWNER MENU 」
+│ ⬡ ${prefix}owner
+│ ⬡ ${prefix}sewabot
+│ ⬡ ${prefix}bc *teks*
+│ ⬡ ${prefix}report
+└──── *[ ᴼᶜᴴᴼᴮᴼᵀᶻ☽ ]* ──────❏
+	
+Note : Semisal Button ga muncul
+*Ketik ${prefix}allmenu*
+*Next Update* Bakal Di Tambahin Fitur Bagus *ᴼᶜᴴᴼᴮᴼᵀᶻ☽*`}
---
+++ message/mess.js
@@ -0,0 +1,15 @@
+module.exports.mess = {
+	        wait: '*Sabar Lagi Proses Tod...!*',
+			success: '*Sukses dilakukan!*',
+			error: {
+				stick: '*Gagal Convert To Sticker...Coba Lagi !*',
+				Iv: '*Linknya Error Tod !*'
+			},
+			only: {
+				group: '*Khusus Grup Bro!*',
+				admin: '*Khusus Admin Bro!*',
+				owner: '*Khusus Owner Bro!*',
+				ownerB: '*Khusus Owner Bot!*',
+				Badmin: '*Make the bot a group admin!*'
+			}
+		}
